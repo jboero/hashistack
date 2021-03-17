d4a0907661702d987dc97188b623be6d211b03e5
jboero <boeroboy@gmail.com>
2021-03-17 18:39:38 +0000

    Migrate to HCL2

--- http/install.sh
+++ http/install.sh
@@ -1,44 +1,37 @@
 #!/bin/bash -x
-cd /root
-setenforce 0
 mv /tmp/replicated.conf /etc
 
-# COPR repo version (open source)
-#yes | dnf copr enable boeroboy/hashicorp
+# For EL8 with Docker:
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Hashicorp official repo
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-
-# For EL8:
-#yes | dnf -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
-#dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-#yes | dnf install -y docker-ce-3:19.03.11-3.el7 packer consul vault nomad terraform --nobest
-
-# Packer requires COPR release.
-yes | dnf install -y docker consul-enterprise vault-enterprise nomad-enterprise
+# Uncomment remaining packages as needed.
+yes | dnf install -y docker-ce # packer consul vault nomad terraform
+systemctl enable --now docker
 
 crontab /tmp/crontab
 
-# RHEL 8 Support for TFE/Docker
-#dnf -y install grubby
-#grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
-#reboot
-#dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-#dnf install docker-ce
+# Try not to disable SELinux
+#sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 
-# Settings for Terraform Enterprise:
-sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
 # Flush all firewall rules to start fresh
-iptables -F
-iptables-save
-firewall-cmd --permanent --zone=trusted --add-interface=docker0
-firewall-cmd --zone=public --permanent --add-port=4600-4700/tcp
-firewall-cmd --zone=public --permanent --add-port=8300-8800/tcp
+#iptables -F
+#iptables-save
+#firewall-cmd --permanent --zone=trusted --add-interface=docker0
+#firewall-cmd --zone=public --permanent --add-port=4600-4700/tcp
+#firewall-cmd --zone=public --permanent --add-port=8300-8700/tcp
+firewall-cmd --zone=public --permanent --add-port=8800/tcp
 firewall-cmd --zone=public --permanent --add-port=443/tcp
 firewall-cmd --reload
-systemctl enable --now docker
+
 sed -E -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-curl -sSL -o /tmp/replicated.sh -o install.sh https://get.replicated.com/docker
-chmod +x /tmp/replicated.sh
-/tmp/replicated.sh < /tmp/answers
+
+curl -sSL -o /tmp/replicated.sh https://install.terraform.io/ptfe/stable
+#curl -sSL -o /tmp/replicated.sh -o install.sh https://get.replicated.com/docker
+
+#/tmp/replicated.sh < /tmp/answers
+export privip=$(ip a show dev eth0 | awk '/inet / {print $2}' |cut -d/ -f1)
+bash -x /tmp/replicated.sh no-proxy no-docker \
+    public-address="$privip" private-address="$privip" \
+    bypass-firewalld-warning 
+
